a4157b7e8ac4984f477626a9c6d2f98d085b8608
AltruisticCraftLab <altruisticcraftlab@gmail.com>
2025-06-06 01:34:00 +0530

    Change the logo.tsx file for high priority image loading

--- nozips/logo.tsx
+++ nozips/logo.tsx
@@ -1,12 +1,10 @@
-//  src/components/logo.tsx
-
 import Image from "next/image";
 import Link from "next/link";
 
 export const Logo = () => {
   return (
     <Link href="/">
-      <Image src="logo.svg" alt="logo" width={40} height={40} />
+      <Image src="logo.svg" alt="logo" width={40} height={40} priority />
     </Link>
   );
 };
